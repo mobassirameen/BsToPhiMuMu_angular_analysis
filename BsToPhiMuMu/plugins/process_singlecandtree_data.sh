811246ba52889feab74431d86694cb61adadd938
rishabh-raturi <rishabh.raturi@cern.ch>
2020-12-30 10:23:43 +0100

    data update

--- BsToPhiMuMu/plugins/process_singlecandtree_data.sh
+++ BsToPhiMuMu/plugins/process_singlecandtree_data.sh
@@ -12,15 +12,15 @@ echo -e "\n=> processing singlecand ntuple for Bs2phimm sample."
 #./sel data DoubleMuonLowMass cut_bdt root://cmseos.fnal.gov//eos/uscms/store/user/ckar/crab_processed/DoubleMuonLowMass_2016E_MINI_Correction_Iso.root  DoubleMuonLowMass_2016E_Mini_Iso -s 0  -n 100000
 #./sel data DoubleMuonLowMass cut_bdt root://cmseos.fnal.gov//eos/uscms/store/user/ckar/crab_processed/DoubleMuonLowMass_2016F_MINI_Correction_Iso.root  DoubleMuonLowMass_2016F_Mini_Iso -s 0  -n 100000
 #./sel data DoubleMuonLowMass cut_bdt root://cmseos.fnal.gov//eos/uscms/store/user/ckar/crab_processed/DoubleMuonLowMass_2016G_MINI_Correction_Iso.root  DoubleMuonLowMass_2016G_Mini_Iso -s 0  -n 100000
-./sel data DoubleMuonLowMass cut_bdt root://cmseos.fnal.gov//eos/uscms/store/user/ckar/crab_processed/DoubleMuonLowMass_2016H_MINI_Correction_Iso.root  DoubleMuonLowMass_2016H_Mini_Iso -s 0  -n 2000000
+#./sel data DoubleMuonLowMass cut_bdt root://cmseos.fnal.gov//eos/uscms/store/user/ckar/crab_processed/DoubleMuonLowMass_2016H_MINI_Correction_Iso.root  DoubleMuonLowMass_2016H_Mini_Iso -s 0  -n 2000000
 
 #./sel data Charmonium cut_bdt root://cmseos.fnal.gov//eos/uscms/store/user/ckar/crab_processed/Charmonium_2016B_MINI_Correction_Iso.root Charmonium_2016B_Mini_Iso -s 0  -n 100000
 #./sel data Charmonium cut_bdt root://cmseos.fnal.gov//eos/uscms/store/user/ckar/crab_processed/Charmonium_2016C_MINI_Correction_Iso.root Charmonium_2016C_Mini_Iso -s 0  -n 100000
 #./sel data Charmonium cut_bdt root://cmseos.fnal.gov//eos/uscms/store/user/ckar/crab_processed/Charmonium_2016D_MINI_Correction_Iso.root Charmonium_2016D_Mini_Iso -s 0  -n 100000
-#./sel data Charmonium cut_bdt root://cmseos.fnal.gov//eos/uscms/store/user/ckar/crab_processed/Charmonium_2016E_MINI_Correction_Iso.root Charmonium_2016E_Mini_Iso -s 0  -n 100000
-#./sel data Charmonium cut_bdt root://cmseos.fnal.gov//eos/uscms/store/user/ckar/crab_processed/Charmonium_2016F_MINI_Correction_Iso.root Charmonium_2016F_Mini_Iso -s 0  -n 100000
-#./sel data Charmonium cut_bdt root://cmseos.fnal.gov//eos/uscms/store/user/ckar/crab_processed/Charmonium_2016G_MINI_Correction_Iso.root Charmonium_2016G_Mini_Iso -s 0  -n 100000
-#./sel data Charmonium cut_bdt root://cmseos.fnal.gov//eos/uscms/store/user/ckar/crab_processed/Charmonium_2016H_MINI_Correction_Iso.root Charmonium_2016H_Mini_Iso -s 0  -n 100000
+./sel data Charmonium cut_bdt root://cmseos.fnal.gov//eos/uscms/store/user/ckar/crab_processed/Charmonium_2016E_MINI_Correction_Iso.root Charmonium_2016E_Mini_Iso -s 0  -n 2000000
+./sel data Charmonium cut_bdt root://cmseos.fnal.gov//eos/uscms/store/user/ckar/crab_processed/Charmonium_2016F_MINI_Correction_Iso.root Charmonium_2016F_Mini_Iso -s 0  -n 2000000
+./sel data Charmonium cut_bdt root://cmseos.fnal.gov//eos/uscms/store/user/ckar/crab_processed/Charmonium_2016G_MINI_Correction_Iso.root Charmonium_2016G_Mini_Iso -s 0  -n 2000000
+./sel data Charmonium cut_bdt root://cmseos.fnal.gov//eos/uscms/store/user/ckar/crab_processed/Charmonium_2016H_MINI_Correction_Iso.root Charmonium_2016H_Mini_Iso -s 0  -n 2000000
 
 
 # ./sel data DoubleMuonLowMass cut0 /uscms/home/ckar/nobackup/0000/DoubleMuonLowMass_2016B_MINI.root  DoubleMuonLowMass_2016B_Mini_Presel -s 0  -n 2000000
